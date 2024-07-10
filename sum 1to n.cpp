88d476a4107eafb2d2bdc26092d2b989be6dde16
Pratyusha <168525135+Pratyusha4788@users.noreply.github.com>
2024-07-10 23:33:11 +0530

    Add files via upload

---
+++ sum 1to n.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+
+    int sum =0;
+
+    for (int i = 1; i <= n; i++)
+    {
+         sum =sum +i;
+        /* code */
+    }
+    cout<<sum<<endl; 
+    
+    /* code */
+
+    return 0;
+}
