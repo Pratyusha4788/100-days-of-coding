02c990a19ee21263a883ad1979f88787740b4081
Pratyusha <168525135+Pratyusha4788@users.noreply.github.com>
2024-07-13 15:58:03 +0530

    Add files via upload

---
+++ intersectionOfArray.cpp
@@ -0,0 +1,30 @@
+//intersection-remove all common element -output
+//if no intersection then -1
+//sorted in non decreasing order
+
+
+#include<vector>
+using namespace std;
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	int i = 0, j = 0;
+    vector<int> ans;
+    while(i<n && j<m) {
+    
+    if(arr1[i]==arr2[j])
+    {
+    	ans.push_back(arr1[i]);
+        i++;
+        j++;
+    }
+    else if (arr1[i] < arr2[j]) {
+  		i++;
+    }
+    else
+    {
+    	j++;
+    }
+    
+    }
+    return ans;
+}
